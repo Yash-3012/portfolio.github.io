28816a2bb59c755ee7e7315119c628707c28de9e
Yash-3012 <yashptl051230@gmail.com>
2025-11-02 19:29:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
+
+// Contact form alert
+document.querySelector(".contact-form").addEventListener("submit", e => {
+  e.preventDefault();
+  alert("Thank you for contacting me! I’ll get back to you soon.");
+});
